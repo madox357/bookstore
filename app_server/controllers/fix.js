660c59206b5d532852eecbb1959472a3ee78f329
Bijaya Limbu <bl18aat@herts.ac.uk>
2020-01-18 11:16:22 +0000

    Removed inputStatus & added more commenting

--- app_server/controllers/fix.js
+++ app_server/controllers/fix.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var apiOptions = {
-    server : "http://localhost:3000"    /**Set default server URL for local development */
+    server : "http://localhost:3000"
 };
 
 var renderFixList = function(req, res, body){
